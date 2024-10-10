d7f9ff561878efbb94ac5fe6f3a601a4ee13083c
Vaibhav Srivastav <reach-vb@users.noreply.huggingface.co>
2024-10-10 13:38:41 +0000

    Update app.py (#2)
    - Update app.py (327f3aa8c6e3cd2e95f790a24e596a48598e88a6)

--- app.py
+++ app.py
@@ -86,48 +86,6 @@ def process_model(model_id, q_method, private_repo, oauth_token: gr.OAuthToken |
         card.text = dedent(
             f"""
             # {new_repo_id}
-            This model was converted to GGUF format from [`{model_id}`](https://huggingface.co/{model_id}) using llama.cpp via the ggml.ai's [GGUF-my-repo](https://huggingface.co/spaces/ggml-org/gguf-my-repo) space.
-            Refer to the [original model card](https://huggingface.co/{model_id}) for more details on the model.
-            
-            ## Use with llama.cpp
-            Install llama.cpp through brew (works on Mac and Linux)
-            
-            ```bash
-            brew install llama.cpp
-            
-            ```
-            Invoke the llama.cpp server or the CLI.
-            
-            ### CLI:
-            ```bash
-            llama-cli --hf-repo {new_repo_id} --hf-file {quantized_gguf_name} -p "The meaning to life and the universe is"
-            ```
-            
-            ### Server:
-            ```bash
-            llama-server --hf-repo {new_repo_id} --hf-file {quantized_gguf_name} -c 2048
-            ```
-            
-            Note: You can also use this checkpoint directly through the [usage steps](https://github.com/ggerganov/llama.cpp?tab=readme-ov-file#usage) listed in the Llama.cpp repo as well.
-
-            Step 1: Clone llama.cpp from GitHub.
-            ```
-            git clone https://github.com/ggerganov/llama.cpp
-            ```
-
-            Step 2: Move into the llama.cpp folder and build it with `LLAMA_CURL=1` flag along with other hardware-specific flags (for ex: LLAMA_CUDA=1 for Nvidia GPUs on Linux).
-            ```
-            cd llama.cpp && LLAMA_CURL=1 make
-            ```
-
-            Step 3: Run inference through the main binary.
-            ```
-            ./llama-cli --hf-repo {new_repo_id} --hf-file {quantized_gguf_name} -p "The meaning to life and the universe is"
-            ```
-            or 
-            ```
-            ./llama-server --hf-repo {new_repo_id} --hf-file {quantized_gguf_name} -c 2048
-            ```
             """
         )
         card.save(f"README.md")
