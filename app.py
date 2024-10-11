ac97e5b313aa27669f251fb12ad62998e41094cc
Vaibhav Srivastav <reach-vb@users.noreply.huggingface.co>
2024-10-11 08:46:44 +0000

    Update app.py (#15)
    - Update app.py (4fb64a2876f7880976b7db6feebe48e727684449)

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ from huggingface_hub import whoami
 from huggingface_hub import ModelCard
 from huggingface_hub import login
 from huggingface_hub import scan_cache_dir
+from huggingface_hub import logging
 
 from gradio_huggingfacehub_search import HuggingfaceHubSearch
 
@@ -18,7 +19,9 @@ from apscheduler.schedulers.background import BackgroundScheduler
 
 from textwrap import dedent
 
-from mlx_lm import convert
+from mlx_lm import convert, __version__
+
+from typing import Any, Callable, Dict, Generator, List, Optional, Tuple, Type, Union
 
 HF_TOKEN = os.environ.get("HF_TOKEN")
 
@@ -33,6 +36,55 @@ def clear_cache():
 
     print("Cache has been cleared")
 
+def upload_to_hub(path, upload_repo, hf_path, token):
+    
+    card = ModelCard.load(hf_path)
+    card.data.tags = ["mlx"] if card.data.tags is None else card.data.tags + ["mlx"]
+    card.data.base_model = hf_path
+    card.text = dedent(
+        f"""
+        # {upload_repo}
+
+        The Model [{upload_repo}](https://huggingface.co/{upload_repo}) was converted to MLX format from [{hf_path}](https://huggingface.co/{hf_path}) using mlx-lm version **{__version__}**.
+
+        ## Use with mlx
+
+        ```bash
+        pip install mlx-lm
+        ```
+
+        ```python
+        from mlx_lm import load, generate
+
+        model, tokenizer = load("{upload_repo}")
+
+        prompt="hello"
+
+        if hasattr(tokenizer, "apply_chat_template") and tokenizer.chat_template is not None:
+            messages = [{{"role": "user", "content": prompt}}]
+            prompt = tokenizer.apply_chat_template(
+                messages, tokenize=False, add_generation_prompt=True
+            )
+
+        response = generate(model, tokenizer, prompt=prompt, verbose=True)
+        ```
+        """
+    )
+    card.save(os.path.join(path, "README.md"))
+
+    logging.set_verbosity_info()
+
+    api = HfApi()
+    api.create_repo(repo_id=upload_repo, exist_ok=True)
+    api.upload_folder(
+        folder_path=path,
+        repo_id=upload_repo,
+        repo_type="model",
+        multi_commits=True,
+        multi_commits_verbose=True,
+    )
+    print(f"Upload successful, go to https://huggingface.co/{upload_repo} for details.")    
+
 def process_model(model_id, q_method,oauth_token: gr.OAuthToken | None):
     
     if oauth_token.token is None:
@@ -41,11 +93,12 @@ def process_model(model_id, q_method,oauth_token: gr.OAuthToken | None):
     model_name = model_id.split('/')[-1]
     username = whoami(oauth_token.token)["name"]
 
-    login(token=oauth_token.token, add_to_git_credential=True)
+    # login(token=oauth_token.token, add_to_git_credential=True)
     
     try:
         upload_repo = username + "/" + model_name + "-mlx"
-        convert(model_id, quantize=True, upload_repo=upload_repo)
+        convert(model_id, quantize=True)
+        upload_repo(path="mlx_model", upload_repo=upload_repo, hf_path=repo_id, token=oauth_token.token)
         clear_cache()
         return (
             f'Find your repo <a href=\'{new_repo_url}\' target="_blank" style="text-decoration:underline">here</a>',
